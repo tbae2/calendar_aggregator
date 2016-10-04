7eaa11938b257117b03c21b0d1597f8e7c07c69c
tbae2 <=>
2016-10-04 15:03:00 -0400

    modified scraper to create objects that match mongoose schema

--- scrape.js
+++ scrape.js
@@ -4,6 +4,8 @@ const cheerio = require('cheerio');
 // const mongodb = require('mongodb');
 // const mongoconnect = require('./mongoconnect');
 
+
+
 const header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
 };
@@ -28,7 +30,7 @@ urls.forEach(function(cal) {
         //console.log(html);
 
         var holdEvents = [];
-        var calDayData = {};
+        var calData = {};
         var holdDayEvents = [];
         var $ = cheerio.load(html);
         //var testData = $('.ai1ec-day').find('.ai1ec-event-title').text;
@@ -36,16 +38,17 @@ urls.forEach(function(cal) {
         if (cal.id === 'roswell') {
 
             $('.calendar_day_with_items').each(function(i, element) {
-
+                    calData.calendarId = 'roswell';
                 //bind this to var for easier reuse
                 var element = $(this);
                 //pick out the day which is a text in the div without all the other elements, trim the whitespace
                 var calDay = Number(element[0].childNodes[0].nodeValue.trim());
-                calDayData.day = calDay;
+                calData.day = calDay;
 
                 element.find('.calendar_item').each(function(i, item) {
                     //build each event into an object and then push to holdDayEvents Array to aggregate
                     var buildEvent = {};
+                    buildEvent.eventNumber = i;
                     buildEvent.time = $(this).find('.calendar_eventtime').text().trim();
                     buildEvent.title = $(this).find('.calendar_eventlink').text().trim();
                     buildEvent.url = 'http://www.roswellgov.com' + $(this).find('.calendar_eventlink').attr('href');
@@ -61,28 +64,31 @@ urls.forEach(function(cal) {
         };
         if (cal.id === 'marietta') {
 
-            $('.ai1ec-day').each(function(i, element) {
+          calData.calendarId = 'marietta';
 
+            $('.ai1ec-day').each(function(i, element) {
 
                 var item = $(this);
                 var calDay = item.find('.ai1ec-date').text().trim();
-                calDayData.day = calDay;
+                calData.day = calDay;
                 item.find('.ai1ec-event').each(function(i, dayevent) {
-                    var buildEvent = {};
-
+                      var buildEvent = {};
+                    buildEvent.eventNumber = i;
                     buildEvent.time = $(this).find('.ai1ec-event-time').text().trim();
                     buildEvent.title = $(this).find('.ai1ec-event-title').text().trim();
                     buildEvent.url = $(this).closest('.ai1ec-event-container').attr('href');
                     holdDayEvents.push(buildEvent);
                 });
 
-                calDayData.event = holdDayEvents;
-                holdEvents.push(calDayData);
+            calData.eventData = holdDayEvents;
+            holdDayEvents = [];
+              //  holdEvents.push(calDayData);
 
             });
 
         }
         //console.log(holdEvents);
+        console.log(calData);
         // mongoconnect(cal.id, holdEvents);
         holdEvents = [];
     });
